8e5d1787bb67ac77050a5aa62019b51bcab2ea12
sam <sam.zhang@3ti.us>
2018-03-30 00:39:44 +0800

    second

--- meadowlark.js
+++ meadowlark.js
@@ -1,17 +1,39 @@
+function getWeatherData() {
+    return {
+        locations: [{
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 var app = express();
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
 
 var handlebars = require('express-handlebars')
     .create({
-        defaultLayout: 'main'
+        defaultLayout: 'main',      
+        layoutDir: app.get('views') + '/layouts',
+        partialsDir:[app.get('views') + '/partials']  
     });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -21,13 +43,25 @@ app.set('port', process.env.PORT || 3000);
 //静态文件目录
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
+app.use(function(req,res,next){
+    if(!res.locals.partials) res.locals.partials ={};
+    res.locals.partials.weather  = getWeatherData();
+    next();
+});
+
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
+
+app.get('/home', function (req, res) {
     res.render('home');
 });
 app.get('/about', function (req, res) {
-    var randoeFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
     res.render('about', {
-        fortune: randoeFortune
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
     });
 });
 
